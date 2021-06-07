5e23260eed389f45c2713b50f21062ebafe175fe
Clayton Campos <clayton.almeida.campos@gmail.com>
2021-06-07 10:10:12 -0300

    update certificates and profile picture

--- pages/index.js
+++ pages/index.js
@@ -36,11 +36,23 @@ export default function Home({ allPostsData }) {
 					melhor forma possível.
 					<br />
 					<h4>Cursos Concluidos (Certificados)</h4>
+					Bootcamp SPTesch Dessenvolvimento FullStack 109 Hrs (DIO)
+					<br />
+					DevOps Essentials (4Linux)
+					<br />
+					Linux Beginners in Clound (4Linx)
+					<br />
+					Customer Experience – 40hrs (FIAP)
+					<br />
+					Design Thinking – 40hrs (FIAP)
+					<br />
 					User Experience – 60hrs (FIAP)
 					<br />
 					Estação Hack Facebook - Front End Coding - 105 Hrs (Digital House)
 					<br />
 					Web Moderno 2020 - 87,5 Hrs (Udemy)
+					<br />
+					Web Developer 120 Hrs (DIO)
 					<br />
 					Bootcamp SPTech Desenvolvimento Front-end 82Hrs (DIO)
 					<br />
@@ -82,3 +94,17 @@ export default function Home({ allPostsData }) {
 		</Layout>
 	)
 }
+<br />
+
+- 
+- User Experience – 60hrs (FIAP)
+- Estação Hack Facebook - Front End Coding - 105 Hrs (Digital House)
+- Web Moderno 2020 - 87,5 Hrs (Udemy)
+- Web Developer 120 Hrs (DIO)
+- Bootcamp React Web Developer - 80 Hrs (DIO)
+- Bootcamp HTML Web Developer - 27 Hrs (DIO)
+- Bootcamp Avanade Decola Dev 2021 - 85 Hrs (DIO)
+- Bootcamp GFT START_UNI 2021 - 46 Hrs (DIO)
+- Bootcamp SPTech Desenvolvimento Front-end 82Hrs (DIO)
+- Scrum Foundation SFPC - (CertiProf) 
+- Lógica de Programação 14Hrs (SENAI)
